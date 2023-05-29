59835e5ff691fb445130017907f13998c3f5e39a
fengsonglin <fengsonglin@happy-seed.com>
2023-05-29 16:14:41 +0800

    feat: naviation_bar的创建

---
+++ flutter_next_douban/lib/module/navigation_bar/navigation_bar.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_next_douban/module/home/home_page.dart';
+import 'package:flutter_next_douban/module/person/person_page.dart';
+
+class NavigationBar1 extends StatefulWidget {
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar1> {
+  int _selectedIndex = 0;
+  PageController _pageController = PageController();
+  void _onItemTapped(int index) {
+    _pageController.animateToPage(index,
+        duration: const Duration(milliseconds: 300), curve: Curves.ease);
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[HomePage(), PersonPage()],
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '我的'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
